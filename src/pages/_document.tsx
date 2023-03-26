7d6b984ecf47e1edb14225a0dacfd7f472ca2f03
segersniels <segers.n@hotmail.com>
2023-03-26 19:05:27 +0200

    :rotating_light: Fix linting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,10 @@ export default function Document() {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>"
         />
 
-        <meta name="description" content="Generate your git commit message using AI" />
+        <meta
+          name="description"
+          content="Generate your git commit message using AI"
+        />
         <meta property="og:site_name" content="commit-message.com" />
         <meta
           property="og:description"
